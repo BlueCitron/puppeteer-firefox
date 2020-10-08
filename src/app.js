e3c4e7d62b8e3d1d1845eb035f47a68eca0ab5f2
Dong Seong, Seo <=>
2020-10-08 13:19:25 +0900

    test

--- src/app.js
+++ src/app.js
@@ -2,19 +2,22 @@ const puppeteer = require('puppeteer');
 
 
 (async () => {
+
+    const browserFetcher = puppeteer.createBrowserFetcher({ product: 'firefox' });
+    const revisionInfo = await browserFetcher.download('83.0a1');
     const browser = await puppeteer.launch({
         product: 'firefox',
         headless: false,
         slowMo: 60,
         defaultViewport: { width: 800, height: 800 }, // 엘리먼트를 찾아서 스크립트 작업을 하는경우 필요.
         args: ["--no-sandbox", "--disable-gpu", "--disable-web-security", "--wait-for-process"],
-        // executablePath: '/usr/bin/chromium-browser'
+        executablePath: revisionInfo.executablePath,
     });
 
 
-    const page = browser.newPage();
+    const page = await browser.newPage();
 
-    await page.goto('www.google.com');
+    await page.goto('https://www.google.com');
 
     console.log('moved to google.');
 
